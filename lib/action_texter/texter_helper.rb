bc6e7ef0c7cafa4162caa425b2a6a227d4f052a9
David Kuo <s50407s@gmail.com>
2016-07-06 23:35:29 +0800

    Update helper

--- lib/action_texter/texter_helper.rb
+++ lib/action_texter/texter_helper.rb
@@ -1,31 +1,7 @@
 module ActionTexter
   # Provides helper methods for ActionTexter::Base that can be used for easily
-  # formatting messages, accessing texter or message instances.
+  # accessing texter or message instances.
   module TexterHelper
-    # Take the text and format it, indented two spaces for each line, and
-    # wrapped at 72 columns:
-    #
-    #   text = <<-TEXT
-    #     This is
-    #     the      paragraph.
-    #
-    #     * item1 * item2
-    #   TEXT
-    #
-    #   block_format text
-    #   # => "  This is the paragraph.\n\n  * item1\n  * item2\n"
-    def block_format(text)
-      formatted = text.split(/\n\r?\n/).collect { |paragraph|
-        format_paragraph(paragraph)
-      }.join("\n\n")
-
-      # Make list points stand on their own line
-      formatted.gsub!(/[ ]*([*]+) ([^*]*)/) { "  #{$1} #{$2.strip}\n" }
-      formatted.gsub!(/[ ]*([#]+) ([^#]*)/) { "  #{$1} #{$2.strip}\n" }
-
-      formatted
-    end
-
     # Access the texter instance.
     def texter
       @_controller
@@ -34,31 +10,6 @@ module ActionTexter
     # Access the message instance.
     def message
       @_message
-    end
-
-    # Returns +text+ wrapped at +len+ columns and indented +indent+ spaces.
-    # By default column length +len+ equals 72 characters and indent
-    # +indent+ equal two spaces.
-    #
-    #   my_text = 'Here is a sample text with more than 40 characters'
-    #
-    #   format_paragraph(my_text, 25, 4)
-    #   # => "    Here is a sample text with\n    more than 40 characters"
-    def format_paragraph(text, len = 72, indent = 2)
-      sentences = [[]]
-
-      text.split.each do |word|
-        if sentences.first.present? && (sentences.last + [word]).join(' ').length > len
-          sentences << [word]
-        else
-          sentences.last << word
-        end
-      end
-
-      indentation = " " * indent
-      sentences.map! { |sentence|
-        "#{indentation}#{sentence.join(' ')}"
-      }.join "\n"
     end
   end
 end
